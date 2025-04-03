9280412f6cb94e62c6712d7b5ef7f3c5c0b88907
Eze Victor Expounder <victorexpounder@gmail.com>
2025-04-03 20:08:10 +0100

    trying to reduce memory usage

--- server.py
+++ server.py
@@ -15,6 +15,12 @@ app = Flask(__name__)
 CORS(app)
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode="threading")
 
+# Prevent TensorFlow from using all memory
+gpus = tf.config.experimental.list_physical_devices("GPU")
+if gpus:
+    for gpu in gpus:
+        tf.config.experimental.set_memory_growth(gpu, True)
+        
 # Load the trained model
 model = tf.keras.models.load_model("gesture_model.h5")
 GESTURES = ["handOpen", "ThumbsUp", "peace", "fuck", "handClose", "rock", "left", "right"]
